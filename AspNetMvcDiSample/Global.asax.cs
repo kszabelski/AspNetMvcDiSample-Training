e4e0893439a1405786c45901af2236de8bb95c81
Krzysztof Szabelski <kszabelski@gmail.com>
2014-09-23 14:07:28 +0200

    Implement DI using Autofac and Autofac.Mvc4

--- AspNetMvcDiSample/Global.asax.cs
+++ AspNetMvcDiSample/Global.asax.cs
@@ -8,6 +8,8 @@ using System.Web.Routing;
 using System.Web.UI;
 using AspNetMvcDiSample.Controllers;
 using AspNetMvcDiSample.Models;
+using Autofac;
+using Autofac.Integration.Mvc;
 
 namespace AspNetMvcDiSample
 {
@@ -20,12 +22,26 @@ namespace AspNetMvcDiSample
         {
             AreaRegistration.RegisterAllAreas();
 
-            ControllerBuilder.Current.SetControllerFactory(new DiControllerFactory());
+            ConfigureDependencyInjectionContainer();
        
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private static void ConfigureDependencyInjectionContainer()
+        {
+            var containerBuilder = new ContainerBuilder();
+
+            containerBuilder.RegisterType<InMemoryExchangeRateRepository>().As<IExchangeRateRepository>();
+            containerBuilder.RegisterType<CurrencyCalculator>().As<ICurrencyCalculator>();
+            containerBuilder.RegisterAssemblyTypes(typeof(HomeController).Assembly)
+                .Where(t => t.Name.EndsWith("Controller"));
+
+            var container = containerBuilder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
         }
     }
 }
